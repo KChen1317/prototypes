6321f4c7eaf18fdcf636ba3d05c032823f3e280c
KChen1317 <51167526+KChen1317@users.noreply.github.com>
2020-03-12 22:27:16 -0400

    work on system generation
    done with:
        bodies
        planets
        objects
    started:
        colonies
    not done:
        POI
        FTL
        bundling data
    still need:
        XML generation
        file creation/handling

--- system XML generator v.0.0.0.py
+++ system XML generator v.0.0.0.py
@@ -1,6 +1,7 @@
 ####     INSECURE    ####
 
 import xml.dom
+import random
 
 def main():
     print("Warning \nThis program is vulnerable to attack from malicous files\nbe careful")
@@ -29,7 +30,7 @@ def main():
         exit()
     print("continuing")
     system_setting=set_system()    
-    print(str(system_settings))
+    print(str(system_setting))
     system_gen(file_name,mode,system_setting)
 
 
@@ -69,20 +70,79 @@ def test_file_existence(file_name):
 
 
 def set_system():
+    system_id=input("system id?")
     bodies=input("Bodies?")
     planets=input("planets?")
-    asteroids=input("asteroid?")
+    objects=input("objects?")
     colonies=input("colonies?")
     POI=input("POI?")
-    jump_points("Jump?")
-    result={"bodies":bodies,"planets":planets,"asteroids":asteroids,"colonies":colonies,"POI":POI,"FTL":jump_points}
+    jump_points=input("Jump?")
+    seed=input("Seed?\nmust be int")
+    result={"system id":system_id,"bodies":bodies,"planets":planets,"objects":objects,"colonies":colonies,"POI":POI,"FTL":jump_points,"seed":seed}
     return(result)
 
 
 
 
 def system_gen(target_file,file_mode,settings):
-    asfds=1
+    bodies_amt=int(settings["bodies"])              ### central bodies(eg the Sun,a blackhole,pulsar/s,etc)
+    planets_amt=int(settings["planets"])
+    objects_amt=int(settings["objects"])            ### for things that orbit(eg asteroids)
+    colonies_amt=int(settings["colonies"])          ### stores data abt any colony wheter artfical plaetbound or otherwise
+    POI_amt=int(settings["POI"])                    ### for any other objs that do not have a orbit and is not any other category
+    FTL_amt=int(settings["FTL"])                    ### for any FTL related areas
+    system_id=settings["system id"]
+    bodies={}
+    planets={}
+    objects={}
+    colonies={}
+    POI={}
+    FTL={}
+    i=0
+    seed=int(settings["seed"])
+    random.seed(seed)
+    size=random.randint(50,1000)
+    while i<bodies_amt:
+        i=i+1
+        body_id=str("body "+str(i))
+        body_name=random.randint(0,100)             ###use random inputs here as placeholders
+        body_type=random.randint(0,9)
+        body_extra=random.randint(0,1000)
+        body_data={"name":body_name,"id":body_id,"type":body_type,"extra":body_extra}
+        bodies[str(i)]=body_data
+    print(str(bodies))
+    i=0
+    pi=3.14159265
+    while i<planets_amt:
+        i=i+1
+        planet_id=str("planet "+str(i))
+        planet_name=random.randint(0,100)
+        planet_type=random.randint(0,9)
+        planet_orbit_range=random.randint(20,size)  ###use to get orbit data TODO: make it so orbits are ordered for easier processing
+        planet_orbit_distance=round((pi*2*planet_orbit_range)*(random.randint(0,360000)/360000),6)  ###assume that we only need 6 decimal places -replace with degrees?
+        planet_extra=random.randint(0,1000)
+        planet_data={"name":planet_name,"id":planet_id,"type":planet_type,"orbit range":planet_orbit_range,"orbit place":planet_orbit_distance,"extra":planet_extra}
+        planets[str(i)]=planet_data
+    print(str(planets))
+    i=0
+    while i<objects_amt:
+        i=i+1
+        object_id=str("object "+str(i))
+        object_name=random.randint(0,100)
+        object_type=random.randint(0,9)
+        object_orbit_range=random.randint(30,size)
+        object_orbit_distance=round((pi*2*object_orbit_range)*(random.randint(0,360000)/360000),6)
+        object_extra=random.randint(0,1000)
+        object_data={"name":object_name,"id":object_id,"type":object_type,"orbit range":object_orbit_range,"object place":object_orbit_distance,"extra":object_extra}
+        objects[str(i)]=object_data
+    print(str(objects))
+    i=0
+    while i<colonies_amt:
+        i=i+1
+        
+        
+
+    
 
 
 
